c50e5f057815edec85d629b94199aa9bf398918a
Andrea Tupini <tupini07@gmail.com>
2017-01-11 12:32:16 +0100

    mock data

---
+++ src/r_book/apimock.clj
@@ -0,0 +1,31 @@
+(ns r-book.apimock)
+
+;ratings go from 0 to 100 and are ints
+(def db {
+         :john   {:pizza  21
+                  :sushi  82
+                  :pasta  13
+                  :cake   100
+                  :candys 94
+                  :tea    35
+                  :ham    76}
+         :carl   {:pizza 98
+                  :cake  74
+                  :pasta 92
+                  :fish  73
+                  :ham   76
+                  :soup  33}
+         :alys   {:candy 32
+                  :tea   82
+                  :pasta 67
+                  :suhi  83
+                  :ham   80
+                  :fish  74}
+         :johnny {:pizza  11
+                  :sushi  72
+                  :pasta  3
+                  :cake   90
+                  :candys 84
+                  :tea    25
+                  :ham    66}
+         })
